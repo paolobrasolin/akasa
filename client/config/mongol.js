2cfe313a78f61c1862034693710621c6b58b659c
Paolo Brasolin <paolo.brasolin@gmail.com>
2015-03-15 00:14:45 +0100

    Configured mongol, fixed home

---
+++ client/config/mongol.js
@@ -0,0 +1,6 @@
+Session.set("Mongol", {
+	'collections': ['editableDiagrams','publicDiagrams','viewableDiagrams','showcaseDiagrams','allCells'],
+	'display': false,
+	'opacity_normal': ".7",
+	'opacity_expand': ".9",
+});
